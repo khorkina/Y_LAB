232186a71ff8e44b9357956ec138e69a990f9b13
SimpliLern <marinakhorkina2006@gmail.com>
2024-10-10 11:29:28 +0300

    Frequency

---
+++ Frequency.java
@@ -0,0 +1,25 @@
+package Ylab.HabbitApp;
+
+public enum Frequency {
+    DAILY("ежедневно"),
+    WEEKLY("еженедельно");
+
+    private String description;
+
+    Frequency(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Frequency fromString(String input) throws IllegalArgumentException {
+        for (Frequency frequency : Frequency.values()) {
+            if (frequency.getDescription().equalsIgnoreCase(input)) {
+                return frequency;
+            }
+        }//todo: need handle
+        throw new IllegalArgumentException("Неправильный формат частоты. Допустимые значения: ежедневно, еженедельно.");
+    }
+}
